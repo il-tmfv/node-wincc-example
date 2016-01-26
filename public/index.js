1a92bb296e8f43a060c3e680704247ee0d881572
Ilya Timofeev <il.tmfv@gmail.com>
2016-01-26 15:29:09 +0300

    basic functionality

---
+++ public/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var socket = io.connect('http://192.168.4.121:3000');
+    
+    socket.on('connect', function() {
+        $('#conn-status').html("ONLINE");
+        $('#conn-status').css('color', 'green');
+		
+		var dataTags = document.querySelectorAll('[data-wincc-tag]');
+		for (var i = 0; i < dataTags.length; i++) {
+			socket.emit('reg-tag', dataTags[i].attributes['data-wincc-tag'].value);
+		}       
+    }); 
+
+    socket.on('disconnect', function() {
+        $('#conn-status').html("OFFLINE");
+        $('#conn-status').css('color', 'red');
+    });  
+
+    socket.on('new-tag-value', function (data) {
+		for (var tagName in data) {
+			console.log("Trying to show data for tag " + tagName + " = " + data[tagName]);
+			$("[data-wincc-tag='" + tagName + "']").html(data[tagName]);    
+		}
+    });
+});
